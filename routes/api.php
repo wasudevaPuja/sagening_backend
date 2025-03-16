b06e427847a44fffd5b2cab2b29f03d982c24a52
wasudewa puja <wasudewa223@gmail.com>
2025-03-16 21:41:35 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
